84b30e880303788dc4ffb9648108d9ed57914e3f
jacopograndi <jacopo.grandi@outlook.it>
2022-01-07 10:19:06 +0100

    deleted:    CMakeCache.txt 	deleted:    CMakeFiles/3.22.1/CMakeCCompiler.cmake 	deleted:    CMakeFiles/3.22.1/CMakeCXXCompiler.cmake 	deleted:    CMakeFiles/3.22.1/CMakeDetermineCompilerABI_C.bin 	deleted:    CMakeFiles/3.22.1/CMakeDetermineCompilerABI_CXX.bin 	deleted:    CMakeFiles/3.22.1/CMakeSystem.cmake 	deleted:    CMakeFiles/3.22.1/CompilerIdC/CMakeCCompilerId.c 	deleted:    CMakeFiles/3.22.1/CompilerIdC/a.out 	deleted:    CMakeFiles/3.22.1/CompilerIdCXX/CMakeCXXCompilerId.cpp 	deleted:    CMakeFiles/3.22.1/CompilerIdCXX/a.out 	deleted:    CMakeFiles/Arena.dir/DependInfo.cmake 	deleted:    CMakeFiles/Arena.dir/build.make 	deleted:    CMakeFiles/Arena.dir/cmake_clean.cmake 	deleted:    CMakeFiles/Arena.dir/compiler_depend.internal 	deleted:    CMakeFiles/Arena.dir/compiler_depend.make 	deleted:    CMakeFiles/Arena.dir/compiler_depend.ts 	deleted:    CMakeFiles/Arena.dir/depend.make 	deleted:    CMakeFiles/Arena.dir/flags.make 	deleted:    CMakeFiles/Arena.dir/gst/fxs.c.o 	deleted:    CMakeFiles/Arena.dir/gst/fxs.c.o.d 	deleted:    CMakeFiles/Arena.dir/gst/generate.c.o 	deleted:    CMakeFiles/Arena.dir/gst/generate.c.o.d 	deleted:    CMakeFiles/Arena.dir/gst/gst.c.o 	deleted:    CMakeFiles/Arena.dir/gst/gst.c.o.d 	deleted:    CMakeFiles/Arena.dir/gst/info.c.o 	deleted:    CMakeFiles/Arena.dir/gst/info.c.o.d 	deleted:    CMakeFiles/Arena.dir/gst/map.c.o 	deleted:    CMakeFiles/Arena.dir/gst/map.c.o.d 	deleted:    CMakeFiles/Arena.dir/gst/units.c.o 	deleted:    CMakeFiles/Arena.dir/gst/units.c.o.d 	deleted:    CMakeFiles/Arena.dir/hud/hud.c.o 	deleted:    CMakeFiles/Arena.dir/hud/hud.c.o.d 	deleted:    CMakeFiles/Arena.dir/hud/hud_views.c.o 	deleted:    CMakeFiles/Arena.dir/hud/hud_views.c.o.d 	deleted:    CMakeFiles/Arena.dir/json/jsonparse.c.o 	deleted:    CMakeFiles/Arena.dir/json/jsonparse.c.o.d 	deleted:    CMakeFiles/Arena.dir/link.txt 	deleted:    CMakeFiles/Arena.dir/main.c.o 	deleted:    CMakeFiles/Arena.dir/main.c.o.d 	deleted:    CMakeFiles/Arena.dir/mkb/mkb.c.o 	deleted:    CMakeFiles/Arena.dir/mkb/mkb.c.o.d 	deleted:    CMakeFiles/Arena.dir/net/net.c.o 	deleted:    CMakeFiles/Arena.dir/net/net.c.o.d 	deleted:    CMakeFiles/Arena.dir/progress.make 	deleted:    CMakeFiles/Arena.dir/render/button.c.o 	deleted:    CMakeFiles/Arena.dir/render/button.c.o.d 	deleted:    CMakeFiles/Arena.dir/render/render_text.c.o 	deleted:    CMakeFiles/Arena.dir/render/render_text.c.o.d 	deleted:    CMakeFiles/Arena.dir/test/test.c.o 	deleted:    CMakeFiles/Arena.dir/test/test.c.o.d 	deleted:    CMakeFiles/Arena.dir/test/test_gst.c.o 	deleted:    CMakeFiles/Arena.dir/test/test_gst.c.o.d 	deleted:    CMakeFiles/Arena.dir/umath/intersect.c.o 	deleted:    CMakeFiles/Arena.dir/umath/intersect.c.o.d 	deleted:    CMakeFiles/Arena.dir/umath/vec.c.o 	deleted:    CMakeFiles/Arena.dir/umath/vec.c.o.d 	deleted:    CMakeFiles/CMakeDirectoryInformation.cmake 	deleted:    CMakeFiles/CMakeOutput.log 	deleted:    CMakeFiles/Makefile.cmake 	deleted:    CMakeFiles/Makefile2 	deleted:    CMakeFiles/TargetDirectories.txt 	deleted:    CMakeFiles/cmake.check_cache 	deleted:    CMakeFiles/plibsys.dir/DependInfo.cmake 	deleted:    CMakeFiles/plibsys.dir/build.make 	deleted:    CMakeFiles/plibsys.dir/cmake_clean.cmake 	deleted:    CMakeFiles/plibsys.dir/compiler_depend.make 	deleted:    CMakeFiles/plibsys.dir/compiler_depend.ts 	deleted:    CMakeFiles/plibsys.dir/depend.make 	deleted:    CMakeFiles/plibsys.dir/flags.make 	deleted:    CMakeFiles/plibsys.dir/progress.make 	deleted:    CMakeFiles/progress.marks 	deleted:    Makefile 	deleted:    cmake_install.cmake
    rm

--- CMakeFiles/Arena.dir/umath/intersect.c.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/Arena.dir/umath/intersect.c.o: \
- /home/j/ws/arena/umath/intersect.c /usr/include/stdc-predef.h \
- /home/j/ws/arena/umath/intersect.h
--- CMakeFiles/plibsys.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/plibsys.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
